ce9db46b6b54b74f48034446779ed6f071ef3336
Robinofski <rob-vandenberg@outlook.com>
2020-09-03 22:04:03 +0200

    Updates

---
+++ precache-manifest.569a17c94664bfb7a8a130f6a0f50388.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b966aa55c6547e1f3f1d8b65f2db541",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "604ee2d9a7ebbf3f3afa",
+    "url": "/monsters-rolodex/static/css/main.5ec9f6c7.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "604ee2d9a7ebbf3f3afa",
+    "url": "/monsters-rolodex/static/js/main.747dfaa3.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
